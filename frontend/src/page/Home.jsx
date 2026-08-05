a0a600c4b561568ef2db2175fe180842ec068a2e
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-05 12:29:27 -0400

    feat: added hero component folder

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../sections/header/Header";
+import Hero from "../sections/hero/Hero";
 
 function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
