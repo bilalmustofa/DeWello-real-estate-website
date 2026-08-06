abaa9e4fc23068fefdcb7d46ab44d82774900231
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-06 15:18:56 -0400

    feat: add search bar component folder

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../sections/header/Header";
 import Hero from "../sections/hero/Hero";
+import SearchBar from "../sections/searchBar/SearchBar";
 
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <SearchBar />
     </>
   )
 }
