77628df80b36654c933018dd20d23c31e490285a
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-06 08:52:11 -0400

    feat: implement hero section

--- frontend/src/sections/hero/Hero.jsx
+++ frontend/src/sections/hero/Hero.jsx
@@ -1,10 +1,31 @@
+import heroImg from "../../assets/images/hero-image.png";
 
 function Hero() {
   return (
     <>
-        Hero
+      <section className="flex bg-[#F8F2ED] max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-10">
+          <div className="">
+            <h1 className="font-bold text-4xl sm:text-5xl lg:text-7xl leading-tight">
+              Find Your <br /> Dream Home
+            </h1>
+            <p className="mt-6 text-sm sm:text-base lg:text-lg text-gray-600 max-w-md">
+              Explore our curated selection of exquisite properties tailored to
+              your dream home.
+            </p>
+
+            <button className="mt-8 bg-[#2F1D16] text-white px-6 py-3 rounded-lg hover:bg-[#4A2D22] transition">
+              Sign Up
+            </button>
+          </div>
+
+          <div>
+            <img src={heroImg} alt="hero Img" className="w-full max-w-sm sm:max-w-md lg:max-w-2xl"/>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
