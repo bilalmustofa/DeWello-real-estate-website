167303277bcb1b9f4cc8bfca9b67e2f4c4603a27
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-08 01:45:34 -0400

    feat: add About component and integrate it into Home page

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../sections/about/About";
 import Header from "../sections/header/Header";
 import Hero from "../sections/hero/Hero";
 import SearchBar from "../sections/searchBar/SearchBar";
@@ -10,6 +11,7 @@ function Home() {
       <div className="relative z-10 -mt-5 sm:-mt-8 lg:-mt-16">
         <SearchBar />
       </div>
+      <About />
     </>
   )
 }
